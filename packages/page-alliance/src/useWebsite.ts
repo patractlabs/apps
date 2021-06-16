3310430bfcacc2c11bf515b9003c949afda61b7f
Zhichao Zhang <zzcwoshizz@163.com>
2021-06-16 18:15:29 +0800

    feat: alliance website
    use `api.derive` to get account website

--- packages/page-alliance/src/useWebsite.ts
+++ packages/page-alliance/src/useWebsite.ts
@@ -1,14 +1,14 @@
 // Copyright 2017-2021 @polkadot/app-alliance authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Option } from '@polkadot/types/codec';
-import type { AccountId, Registration } from '@polkadot/types/interfaces';
+import type { DeriveAccountRegistration } from '@polkadot/api-derive/types';
+import type { AccountId } from '@polkadot/types/interfaces';
 
 import { useApi, useCall } from '@polkadot/react-hooks';
 
 export function useWebsite (accountId: AccountId | string): string {
   const { api } = useApi();
-  const identity = useCall<Option<Registration>>(api.query.identity.identityOf, [accountId]);
+  const identity = useCall<DeriveAccountRegistration>(api.derive.accounts.identity, [accountId]);
 
-  return identity?.unwrap().info.web.value.toHuman() as string || '';
+  return identity?.web ?? '';
 }
